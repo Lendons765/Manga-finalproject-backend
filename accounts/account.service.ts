222a87f2079ee448d9d78e0e47e84a1464321ade
Lendons765 <lendonmanga84@gmail.com>
2026-05-22 21:06:21 +0800

    fixing  accounts

--- accounts/account.service.ts
+++ accounts/account.service.ts
@@ -235,7 +235,7 @@ function basicDetails(account: any) {
 async function sendVerificationEmail(account: any, origin: any) {
     let message;
     if (origin) {
-        const verifyUrl = `${origin}/account/verify-email?token=${account.verificationToken}`;
+        const verifyUrl = `${origin}/#/account/verify-email?token=${account.verificationToken}`;
         console.log('VERIFY EMAIL URL:', verifyUrl);
         message = `<p>Please click the below link to verify your email address.</p>
                    <p><a href="${verifyUrl}">${verifyUrl}</a></p>`;
